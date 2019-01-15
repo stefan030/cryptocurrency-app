76976bb33a30d6391b477ce88e728fa8995a522b
stefan <stefanbogdanovic030@gmail.com>
2019-01-15 15:22:48 +0100

    Static data set up and pass to child component

---
+++ src/components/Tickers.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './Tickers.css';
+import Cryptocurrency from './Cryptocurrency';
+
+class Tickers extends Component {
+
+    state = {
+        data: [
+            {
+                id: "bitcoin",
+                name: "Bitcoin",
+                symbol: "BTC",
+                price_usd: "1",
+                percent_change_1h: "0",
+                percent_change_24h: "0",
+                percent_change_7d: "0",
+            },
+            {
+                id: "ethereum",
+                name: "Ethereum",
+                symbol: "ETH",
+                price_usd: "1",
+                percent_change_1h: "0",
+                percent_change_24h: "0",
+                percent_change_7d: "0",
+            },
+            {
+                id: "litecoin",
+                name: "Litecoin",
+                symbol: "LTC",
+                price_usd: "1",
+                percent_change_1h: "0",
+                percent_change_24h: "0",
+                percent_change_7d: "0",
+            }
+        ]
+    };
+
+    render() {
+        var tickers = this.state.data.map((currency) => {
+            return(
+                <Cryptocurrency data={currency} key={currency.id} />
+            )
+        });
+        return(
+            <div className='tickers-container'>
+                <ul className='tickers'>{tickers}</ul>
+                <p>Information updated every minute courtesy of coinmarketcap.com</p>
+            </div>
+        )
+    }
+}
+
+export default Tickers;
